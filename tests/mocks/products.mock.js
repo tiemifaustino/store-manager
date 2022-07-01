8c46a264949c225becd390326455707c75f290e7
Tiemi Faustino <hayashipharma@gmail.com>
2022-07-01 15:47:51 -0300

    :construction: Requisito 2 - testes camada Model feito - #getAllProducts #getProductById

---
+++ tests/mocks/products.mock.js
@@ -0,0 +1,9 @@
+const mocks = {
+  listProductsMock: [
+    { id: 1, name: 'Martelo de Thor' },
+    { id: 2, name: 'Traje de encolhimento' },
+  ],
+  mockObj: { id: 2, name: 'Traje de encolhimento' }
+};
+
+module.exports = mocks;
--- tests/unit/productsModel.test.js
+++ tests/unit/productsModel.test.js
@@ -1,8 +1,9 @@
 const connection = require('../../models/connection');
 const productsModel = require('../../models/productsModel');
 const sinon = require('sinon');
-const { expect, use } = require('chai');
 const chaiAsPromised = require('chai-as-promised');
+const { expect, use } = require('chai');
+const { listProductsMock, mockObj } = require('../mocks/products.mock');
 
 use(chaiAsPromised);
 
@@ -19,5 +20,41 @@ describe('productsModel', () => {
       // ação
       expect(result).to.be.eq(true);
     });
+
+    it('ao mandar um id de produto que não existe deve retornar `false`', async () => {
+      const id = 202
+      sinon.stub(connection, 'execute').resolves(true);
+      const result = await productsModel.exists(id)
+      expect(result).to.be.eq(false);
+    });
   });
+
+  // usando o chaiAsPromised
+  describe('#getAllProducts', () => {
+    it('deve retornar um array com produtos ao consultar o banco', () => {
+      // arranjo
+      sinon.stub(connection, 'execute').resolves([listProductsMock]);
+      // ação/assertiva
+      expect(productsModel.getAllProducts()).to.eventually.be.deep.equal(listProductsMock);
+    });
+
+    it('deve falhar se o connection.execute disparar um erro', () => {
+      sinon.stub(connection, 'execute').rejects();
+      expect(productsModel.getAllProducts()).to.eventually.rejected;
+    });
+  });
+
+  describe('#getProductById', () => {
+    it('deve retornar um objeto ao mandar um id de produto existente', () => {
+      sinon.stub(connection, 'execute').resolves([[mockObj]]);
+      expect(productsModel.getProductById(2)).to.eventually.be.deep.equal(mockObj);
+    });
+
+    it('deve retornar undefined ao mandar um id de produto inexistente', async () => {
+      sinon.stub(connection, 'execute').resolves([[]]);
+      const result = await productsModel.getProductById(202);
+      expect(result).to.be.undefined;
+    });
+  });
+
 });
