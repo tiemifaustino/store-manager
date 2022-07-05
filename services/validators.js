00b89a0fb5f7abfe8191b61bdf0a6e9642e12aa0
Tiemi Faustino <hayashipharma@gmail.com>
2022-07-05 16:19:15 -0300

    :construction: Requisito 6 - construindo validações

--- services/validators.js
+++ services/validators.js
@@ -9,3 +9,8 @@ const runSchema = (schema) => (data) => {
 };
 
 module.exports = { runSchema };
+
+/*
+Source:
+Regex link: https://regex-generator.olafneumann.org/?sampleText=%5B0%5D.&flags=i&onlyPatterns=false&matchWholeLine=false&selection=0%7CSquare%20brackets,1%7CNumber,3%7CCharacter
+*/
