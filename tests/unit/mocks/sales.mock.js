317f663b9b2b355f27bf15b6dde0f155472c7d0d
Tiemi Faustino <hayashipharma@gmail.com>
2022-07-10 10:28:08 -0300

    :heavy_check_mark: feat: testes camada model - salesModel Feito

---
+++ tests/unit/mocks/sales.mock.js
@@ -0,0 +1,8 @@
+const mocks = {
+  mockSale: {
+  saleId: 2,
+  date: "2022-07-07T19:51:45.000Z",
+  productId: 3,
+  quantity: 15
+  }
+}
--- tests/unit/models/salesModel.test.js
+++ tests/unit/models/salesModel.test.js
@@ -6,15 +6,51 @@ use(chaiAsPromised);
 
 const connection = require('../../../models/connection');
 const salesModel = require('../../../models/salesModel');
+const { mockSale } = require('../mocks/sales.mock');
+
 
 describe('salesModel', () => {
   beforeEach(() => sinon.restore());
 
+  describe('#exists', () => {
+    it('deve retornar `true` caso o `connection.query` retorne um item da lista', () => {
+      sinon.stub(connection, 'query').resolves([[{}]]);
+      return expect(salesModel.exists(2)).to.eventually.be.true;
+    });
+
+    it('deve retornar `false` o `connection.query` retorne uma lista vazia', () => {
+      sinon.stub(connection, 'query').resolves([[]]);
+      return expect(salesModel.exists(202)).to.eventually.be.equal(false);
+    });
+
+    it('deve disparar um erro caso o `connection.query` dispare um erro', () => {
+      sinon.stub(connection, 'query').rejects();
+      return expect(salesModel.exists(0)).to.eventually.be.rejected;
+    });
+  });
+
   describe('#add', () => {
-    it('deve retornar o id da venda cadastrada', () => {
+    it('deve retornar o id da venda cadastrada caso dê sucesso', () => {
       const expectedId = 4
       sinon.stub(connection, 'query').resolves([{ insertId: expectedId }]);
       return expect(salesModel.add()).to.eventually.be.deep.equal(expectedId);
+    });
+
+    it('deve disparar um erro caso o `connection.query` dispare um erro', () => {
+      sinon.stub(connection, 'query').rejects();
+      return expect(salesModel.add({})).to.eventually.be.rejected;
+    });
+  });
+
+  describe('#remove', () => {
+    it('deve retornar `undefined` caso sucesso', () => {
+      sinon.stub(connection, 'query').resolves();
+      return expect(salesModel.remove(1)).to.eventually.be.undefined;
+    });
+
+    it('deve disparar um erro caso o `connection.query` dispare', () => {
+      sinon.stub(connection, 'query').rejects();
+      return expect(salesModel.remove(1)).to.eventually.be.rejected;
     });
   });
 
